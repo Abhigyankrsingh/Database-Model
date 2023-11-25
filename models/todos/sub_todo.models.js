01a97ea97c83a435c710fe8e1fd9f05d2d32e279
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-25 20:07:48 +0530

    Code_Updated

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -1,4 +1,7 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
 
 const userSchema =  new mongoose.Schema({})
+
+
+export const User = mongoose.model("User", userSchema)
